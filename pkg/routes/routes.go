d85b5f0b410238c73e72bca99dc0e0c78de9cd1d
pecet3 <pecet3107@gmail.com>
2023-11-01 18:08:15 +0100

    initial

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,1 +1,37 @@
-package route
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+}
+func sendTask(w http.ResponseWriter) {
+
+}
+
+func markTaskDone(w http.ResponseWriter, r *http.Request) {
+
+}
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+func createTask(w http.ResponseWriter, r *http.Request) {
+
+}
+func SetupAndRun() {
+	mux := mux.NewRouter()
+	port := ":5000"
+
+	mux.HandleFunc("/", index)
+
+	mux.HandleFunc("/task/{id}", markTaskDone).Methods("PUT")
+	mux.HandleFunc("/task/{id}", deleteTask).Methods("DELETE")
+	mux.HandleFunc("/task", createTask).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(port, mux))
+}
