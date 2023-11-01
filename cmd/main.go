3a4da61a85a21a8ae3e2cdf736b766a4fdee58ae
pecet3 <pecet3107@gmail.com>
2023-11-01 17:39:23 +0100

    cleanrd up

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	model "github.com/pecet3/go+htmx/pkg/model"
 )
 
 func main() {
-	model.ConnectDb()
+	err := model.ConnectDb()
 
-	// model.CreateTask("test")
-
-	tasks, err := model.GetAllTasks()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println(tasks)
-
 }
