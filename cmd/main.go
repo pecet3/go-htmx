4dcb89939152e20d650001ef710a6e09362ae689
pecet3 <pecet3107@gmail.com>
2023-11-01 16:56:59 +0100

    testing db

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import model "github.com/pecet3/go+htmx/pkg/model"
+import (
+	"fmt"
+
+	model "github.com/pecet3/go+htmx/pkg/model"
+)
 
 func main() {
 	model.ConnectDb()
+
+	// model.CreateTask("test")
+
+	tasks, err := model.GetAllTasks()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(tasks)
+
 }
