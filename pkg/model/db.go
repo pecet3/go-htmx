c26216dabfb80bdb62ca2e34868996cdd15ae6a7
pecet3 <pecet3107@gmail.com>
2023-11-01 16:57:11 +0100

    refactored code

--- pkg/model/db.go
+++ pkg/model/db.go
@@ -2,19 +2,23 @@ package model
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
+var (
+	db *sql.DB
+)
+
 func ConnectDb() {
-	db, err := sql.Open("postgres", "user=user1 host=localhost port=5432 password=haslo dbname=db1 sslmode=disable")
+	var err error
+	db, err = sql.Open("postgres", "user=user1 host=localhost port=5432 password=haslo dbname=db1 sslmode=disable")
 	if err != nil {
 		log.Fatal("error during connection to database")
 
 	}
-	fmt.Println(db)
+
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
